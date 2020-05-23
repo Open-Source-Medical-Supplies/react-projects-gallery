ab729637aa52ae494288e1a1adddd2f7949ca5bb
JoshMLeslie <hello@joshmleslie.com>
2020-05-23 14:39:16 -0400

    Updates

---
+++ precache-manifest.f28bb2784da1860055251528012e66fa.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa41f3f6e6ef6b5492e95d67fdebb9c9",
+    "url": "/react-projects-gallery/index.html"
+  },
+  {
+    "revision": "52937b3e308633743fb0",
+    "url": "/react-projects-gallery/static/css/2.4a9b14b7.chunk.css"
+  },
+  {
+    "revision": "22fae74c122deb256966",
+    "url": "/react-projects-gallery/static/css/main.f3c4647b.chunk.css"
+  },
+  {
+    "revision": "52937b3e308633743fb0",
+    "url": "/react-projects-gallery/static/js/2.0479d5ff.chunk.js"
+  },
+  {
+    "revision": "87725b0d81161e4480521ada43622a69",
+    "url": "/react-projects-gallery/static/js/2.0479d5ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fae74c122deb256966",
+    "url": "/react-projects-gallery/static/js/main.355d5341.chunk.js"
+  },
+  {
+    "revision": "c514c0ae33333b9e5021",
+    "url": "/react-projects-gallery/static/js/runtime-main.bf59598e.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/react-projects-gallery/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/react-projects-gallery/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/react-projects-gallery/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "082d10b663f484bce57255466066c814",
+    "url": "/react-projects-gallery/static/media/primeicons.082d10b6.eot"
+  },
+  {
+    "revision": "701daee424b3c1872720a820cae7ca30",
+    "url": "/react-projects-gallery/static/media/primeicons.701daee4.ttf"
+  },
+  {
+    "revision": "96b27389bdf7ccfd4d1d3a1199b1bd81",
+    "url": "/react-projects-gallery/static/media/primeicons.96b27389.svg"
+  },
+  {
+    "revision": "a890ff7c52932af01dbbe1082ed5b8a8",
+    "url": "/react-projects-gallery/static/media/primeicons.a890ff7c.woff"
+  }
+]);
